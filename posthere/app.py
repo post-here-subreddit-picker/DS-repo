8f33f31b30285ad05bc560d9473f3b5e46491ff0
aguilargallardo <aguilargallardo@users.noreply.github.com>
2020-02-05 10:44:14 -0800

    Updates

--- posthere/app.py
+++ posthere/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from . import predictions
 # from flask_sqlalchemy import SQLAlchemy
+import pickle
 
 
 
 def create_app():
     app = Flask(__name__)
+    model = pickle.load(open('posthere/Model.pkl', 'rb'))
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rpg_db.sqlite3'
     # app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
@@ -26,9 +27,9 @@ def create_app():
             was missing in the request.''')
         else:
             inputs = [str(values) for values in title]
-            predict = predictions.jayden(inputs)
+            prediction = model.predict([inputs])[0]
             message = 'You want to post this here! {}'.format(prediction)
-            return render_template('base.html', message=message, predict=predict)
+            return render_template('base.html', message=message, prediction=prediction)
 
 
     
